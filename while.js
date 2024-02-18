6ae746001e220290766b037adc26ff28e31fa740
imran0249 <imrannisho400@gmail.com>
2024-02-19 01:55:34 +0600

    first commit

---
+++ while.js
@@ -0,0 +1,14 @@
+const number = 7;
+if (number < 10) {
+    console.log('choto ekta number paisi');
+}
+console.log('something new is cooking');
+
+// -----------WHILE---LOOP____________
+let num = 0; 
+while(num < 5){ 
+    console.log('loooooooping', num);
+    // num = num + 1;
+    // num += 1;
+    num++;
+}
